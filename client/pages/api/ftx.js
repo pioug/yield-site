defd056b2d60307daa41e5454092c37e526843e8
Gilles Piou <gilles.piou@gmail.com>
2021-03-17 20:33:15 +0800

    Query average yield over the past 7 days

--- client/pages/api/ftx.js
+++ client/pages/api/ftx.js
@@ -6,19 +6,25 @@ const faunaClient = new faunadb.Client({
 const q = faunadb.query;
 
 export default (req, res) => {
-  return faunaClient
-    .query(
-      q.Mean(
-        q.Map(
-          q.Paginate(q.Match(q.Index("ftx_rates_by_coin_time_desc"), "USD"), {
-            size: 24,
-          }),
-          q.Lambda((time, coin, rate, ref) => rate)
-        )
-      )
-    )
-    .then(function (ret) {
-      const apy = Math.pow(Math.pow(1 + ret.data[0], 24), 365);
-      return res.status(200).json({ apy });
-    });
+  return Promise.all([
+    faunaClient.query(getAverageRate(24)),
+    faunaClient.query(getAverageRate(24 * 7)),
+  ]).then(function ([day, week]) {
+    const past_day_average_apy = Math.pow(Math.pow(1 + day.data[0], 24), 365);
+    const past_week_average_apy = Math.pow(Math.pow(1 + week.data[0], 24), 365);
+    return res
+      .status(200)
+      .json({ past_day_average_apy, past_week_average_apy });
+  });
 };
+
+function getAverageRate(hourCount) {
+  return q.Mean(
+    q.Map(
+      q.Paginate(q.Match(q.Index("ftx_rates_by_coin_time_desc"), "USD"), {
+        size: hourCount,
+      }),
+      q.Lambda((time, coin, rate, ref) => rate)
+    )
+  );
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,13 +2,31 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 
 export default function HomePage() {
-  const [apy, setApy] = useState(null);
+  const [apy, setApy] = useState({});
   useEffect(function () {
     fetch("/api/ftx")
       .then((response) => response.json())
-      .then(function ({ apy }) {
-        setApy(apy);
+      .then(function ({ past_day_average_apy, past_week_average_apy }) {
+        setApy({ past_day_average_apy, past_week_average_apy });
       });
   }, []);
-  return <div>{apy && ((apy - 1) * 100).toFixed(2) + "%"}</div>;
+  return (
+    <>
+      <h1>APY for lending USD on FTX</h1>
+      <div>
+        <p>
+          Past 24h:{" "}
+          {apy.past_day_average_apy &&
+            ((apy.past_day_average_apy - 1) * 100).toFixed(2) + "%"}
+        </p>
+      </div>
+      <div>
+        <p>
+          Past 7d:{" "}
+          {apy.past_week_average_apy &&
+            ((apy.past_week_average_apy - 1) * 100).toFixed(2) + "%"}
+        </p>
+      </div>
+    </>
+  );
 }
