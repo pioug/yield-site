1e162dda4ba1d599257f37456e3a99f76fd15e04
Gilles Piou <gilles.piou@gmail.com>
2021-03-20 17:58:53 +0800

    Add DAI, EUR, USDT on APR chart

--- client/pages/api/ftx.js
+++ client/pages/api/ftx.js
@@ -12,14 +12,58 @@ export default (req, res) => {
 };
 
 export function get_rates() {
+  return Promise.all(
+    ["DAI", "EUR", "USD", "USDT"].map(function (coin) {
+      return get_coin_rates(coin).then(function (data) {
+        return {
+          coin,
+          ...data,
+        };
+      });
+    })
+  ).then(function (results) {
+    const largest_dataset = results
+      .reduce(function (current, dataset) {
+        if (current.length < dataset.past_week_rates.length) {
+          return dataset.past_week_rates;
+        }
+        return current;
+      }, [])
+      .map(([datetime]) => [datetime]);
+    results.forEach(function (data) {
+      if (data.past_day_rates.length < 24) {
+        data.past_day_rates = fill_dataset(
+          data.past_day_rates,
+          24,
+          largest_dataset
+        );
+      }
+      if (data.past_week_rates.length < 24 * 7) {
+        data.past_week_rates = fill_dataset(
+          data.past_week_rates,
+          24 * 7,
+          largest_dataset
+        );
+      }
+    });
+    return results;
+  });
+}
+
+function fill_dataset(array, length, filler) {
+  return filler.slice(0, length - array.length).concat(array);
+}
+
+function get_coin_rates(coin) {
   return faunaClient
-    .query(getRecentRates(24 * 7))
+    .query(getRecentCoinRates(coin, 24 * 7))
     .then(function ({ data: past_week_rates }) {
+      past_week_rates = past_week_rates.reverse();
       past_week_rates.forEach(function (rate) {
         rate[1] = convert_hourly_to_yearly(rate[1]);
       });
 
-      const past_day_rates = past_week_rates.slice(0, 24);
+      const past_day_rates = past_week_rates.slice(-24);
       const past_day_average_apr = getAverageRate(past_day_rates);
       const past_week_average_apr = getAverageRate(past_week_rates);
 
@@ -45,9 +89,9 @@ function getAverageRate(array) {
   );
 }
 
-function getRecentRates(hourCount) {
+function getRecentCoinRates(coin, hourCount) {
   return q.Map(
-    q.Paginate(q.Match(q.Index("ftx_rates_by_coin_time_desc"), "USD"), {
+    q.Paginate(q.Match(q.Index("ftx_rates_by_coin_time_desc"), coin), {
       size: hourCount,
     }),
     q.Lambda((time, coin, rate, ref) => [q.ToString(time), rate])
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,21 +4,36 @@ import { useEffect, useRef, useState } from "react";
 import { get_rates } from "./api/ftx.js";
 
 const chart_color_red = "rgb(255, 99, 132)";
+const chart_color_orange = "rgb(255, 159, 64)";
+const chart_color_yellow = "rgb(255, 205, 86)";
+const chart_color_green = "rgb(75, 192, 192)";
+const chart_color_blue = "rgb(54, 162, 235)";
+const chart_color_purple = "rgb(153, 102, 255)";
+const chart_color_grey = "rgb(201, 203, 207)";
+
+const chart_colors = [
+  chart_color_red,
+  chart_color_orange,
+  chart_color_yellow,
+  chart_color_green,
+  chart_color_blue,
+  chart_color_purple,
+  chart_color_grey,
+];
 
 export async function getStaticProps() {
   const data = await get_rates();
   return {
-    props: data,
+    props: { data },
     revalidate: 600,
   };
 }
 
-export default function HomePage(data) {
+export default function HomePage({ data }) {
   const canvas = useRef(null);
   useEffect(
     function () {
       const context2d = canvas.current.getContext("2d");
-      const reversed_past_week_rates = data.past_week_rates.slice().reverse();
       const formatDate = new Intl.DateTimeFormat(navigator.language, {
         dateStyle: "short",
       }).format;
@@ -29,18 +44,18 @@ export default function HomePage(data) {
       const myChart = new Chart(context2d, {
         type: "line",
         data: {
-          labels: reversed_past_week_rates.map(([time]) => time),
-          datasets: [
-            {
-              label: "Lending rate",
+          labels: data[0].past_week_rates.map(([time]) => time),
+          datasets: data.map(function (d, index) {
+            return {
+              label: d.coin,
               fill: false,
-              backgroundColor: chart_color_red,
-              borderColor: chart_color_red,
-              data: reversed_past_week_rates.map(
+              backgroundColor: chart_colors[index],
+              borderColor: chart_colors[index],
+              data: d.past_week_rates.map(
                 ([, rate]) => Math.round(rate * 10000) / 100
               ),
-            },
-          ],
+            };
+          }),
         },
         options: {
           responsive: true,
@@ -66,7 +81,7 @@ export default function HomePage(data) {
                 display: true,
                 scaleLabel: {
                   display: true,
-                  labelString: "Rate (%)",
+                  labelString: "APR (%)",
                 },
                 ticks: {
                   callback: function (dataLabel) {
@@ -77,12 +92,13 @@ export default function HomePage(data) {
             ],
           },
           tooltips: {
+            mode: 'index',
             callbacks: {
               title: function ([a]) {
                 return formatDateTime(new Date(a.label));
               },
               label: function (a) {
-                return `Lending rate: ${a.value}%`;
+                return `${data[a.datasetIndex].coin}: ${a.value}%`;
               },
             },
           },
@@ -101,24 +117,40 @@ export default function HomePage(data) {
         <title>Earn.fyi</title>
         <link rel="icon" type="image/png" href="/favicon32.png" sizes="32x32" />
       </Head>
-      <h1>APR for lending USD on FTX</h1>
+      <h1>APR for margin lending on FTX</h1>
       <div>
-        <p>
-          Past 24h:{" "}
-          {data.past_day_average_apr &&
-            (data.past_day_average_apr * 100).toFixed(2) + "%"}
-        </p>
-      </div>
-      <div>
-        <p>
-          Past 7d:{" "}
-          {data.past_week_average_apr &&
-            (data.past_week_average_apr * 100).toFixed(2) + "%"}
-        </p>
         <div>
           <canvas ref={canvas} />
         </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>24h</th>
+              <th>7d</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(function ({
+              coin,
+              past_day_average_apr,
+              past_week_average_apr,
+            }) {
+              return (
+                <tr key={coin}>
+                  <td>{coin}</td>
+                  <td>{toReadablePercentage(past_day_average_apr)}</td>
+                  <td>{toReadablePercentage(past_week_average_apr)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </main>
   );
+}
+
+function toReadablePercentage(value) {
+  return (value * 100).toFixed(2) + "%";
 }
